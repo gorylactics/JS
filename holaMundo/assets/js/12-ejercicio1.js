bb4669b2581adce1edae32fb98f4b69b12536d15
adrian <adrian.miranda.r@gmail.com>
2016-12-31 21:18:34 -0300

    insertion value 0 in vars

--- holaMundo/assets/js/12-ejercicio1.js
+++ holaMundo/assets/js/12-ejercicio1.js
@@ -1,16 +1,16 @@
 //programa que pida dos numeros , y que diga cual es el mayor, cual es el menor o si son iguales
 
-var numero1 = parseInt(prompt("ingresa numero"));
-var numero2 = parseInt(prompt("ingresa numero"));
+var numero1 = parseInt(prompt("ingresa primer numero",0));
+var numero2 = parseInt(prompt("ingresa segundo numero",0));
 
 
 if(numero1 == numero2){
-    console.log("son identicos");
+    document.write("son identicos");
 }else if (numero1 > numero2){
-    console.log(numero1 + " es mayor que " +  numero2);
+    document.write(numero1 + " es mayor que " +  numero2);
 }else if (numero1 < numero2){
-    console.log( numero1 + " es menor que " + numero2);
+    document.write( numero1 + " es menor que " + numero2);
 }else{
-    console.log("ingreso de caracteres distintos a numeros");
+    document.write("ingreso de caracteres distintos a numeros");
 };
 
