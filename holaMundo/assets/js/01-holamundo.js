ea546477e89c68d8626805387e9fa43b21f1ca42
gorylactico <adrian.miranda.r@gmail.com>
2018-11-23 00:20:31 -0300

    JS

---
+++ holaMundo/assets/js/01-holamundo.js
@@ -0,0 +1,8 @@
+alert("holamundoconJS");
+alert("vamos que se puede");
+document.write("Comienzo de la aventura en JS a dominarlo se ha dicho ");
+console.log("hoy nos acercamos mas");
+var a = 10
+var b = 2
+console.log(20+2);
+
