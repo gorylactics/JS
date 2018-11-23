9f2588a0da7833f3ee00de66628458f020a371bf
gorylactico <adrian.miranda.r@gmail.com>
2018-11-23 00:25:34 -0300

    now i understand var

---
+++ holaMundo/assets/js/02-variables.js
@@ -0,0 +1,22 @@
+"use strict"
+// "use strict" sirve para que javascript no nos permita codificar de mala manera , y ademas permite la activacion de nuevas funcionalidades en ercm6 o como se llame çXD, usarlo en todos los ficheros con tal de tener una mejor y mas limpia codificacion
+
+var comuna = "santiago";
+
+var calle = "alameda";
+
+var num = "426";
+
+var a = 3;
+var b = 5
+
+var comuna_calle = comuna +" "+ calle;
+
+console.log(calle , num ,comuna , a+b);
+// para concatenar , osea unir dos variables , se debe usar el simbolo ++ y " " entre ellos para dar espacio
+
+alert(comuna_calle,);
+alert("un paso mas 💪🏻 ")
+
+
+
