3f4a95254bae85cbf80fb6567dfbedbdcea23554
adrian <adrian.miranda.r@gmail.com>
2018-11-26 22:50:59 -0300

    loop FOR

---
+++ holaMundo/assets/js/08-bucleFor.js
@@ -0,0 +1,15 @@
+"use strict"
+
+/* Un bucle se conoce como BUBLE , LOOP , ITERADORES , CICLOS 
+
+y un buble es una estructura de control que se repite varias veces , ya sea definido o indefinodo de veces, pero por lo general son definido
+*/
+
+var numero = 100;
+
+for(var i=0; i<= numero ; i++){
+    console.log(i);
+    // debugger;
+};
+
+// existe el comnando debugger que es para tornar el bucle de manera manual
