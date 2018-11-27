85c401a63831d59e36ad8ee8a33cd1f5c9bc8161
adrian <adrian.miranda.r@gmail.com>
2018-11-26 23:04:25 -0300

    loop while

---
+++ holaMundo/assets/js/09-while.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// WHILE significa mientras que ... se ejecutara este codigo
+
+var year = 2018
+
+while(year <= 2051){
+    // ejecuta esto
+    console.log("este es el año " + year);
+    // para que el loop no sea infito , se debe dar la orden de incrementacion  ++ cosa de que sume desde la base de year hasta llegar hasta el dato que estamamos midiendo
+    year++;
+}
+
+// y al reves si queremos ir en reversa , debemos usar el operador de decremento --
