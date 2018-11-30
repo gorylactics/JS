66e3be9bf264c145c8bf1d0537d87d35191d9d11
adrian <adrian.miranda.r@gmail.com>
2018-11-29 23:05:57 -0300

    update info loop

--- holaMundo/assets/js/09-while.js
+++ holaMundo/assets/js/09-while.js
@@ -5,7 +5,7 @@
 var year = 2018
 
 while(year <= 2051){
-    // ejecuta esto
+    // mientras que year sea menor o igual que 2051 ejecutate
     console.log("este es el año " + year);
     // para que el loop no sea infito , se debe dar la orden de incrementacion  ++ cosa de que sume desde la base de year hasta llegar hasta el dato que estamamos midiendo
     year++;
