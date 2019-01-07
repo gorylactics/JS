8478d16a959e0fd06cc48fd3d4e5fe645ca36aac
adrian <adrian.miranda.r@gmail.com>
2019-01-07 19:33:18 -0300

    show numer betwenn

---
+++ holaMundo/assets/js/13-ejercicio3.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var numero1 = parseInt(prompt('ingresa un numero'));
+var numero2 = parseInt(prompt('ingresa un numero'));
+
+for(var i = numero1; i <= numero2 ; i++){
+    console.log(i)
+}
+
