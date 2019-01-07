59fddd9534f983fd1e93eef8a085371a258af902
adrian <adrian.miranda.r@gmail.com>
2019-01-07 18:54:51 -0300

    insertion of 'while' condition

--- holaMundo/assets/js/12-ejercicio1.js
+++ holaMundo/assets/js/12-ejercicio1.js
@@ -3,14 +3,17 @@
 var numero1 = parseInt(prompt("ingresa primer numero",0));
 var numero2 = parseInt(prompt("ingresa segundo numero",0));
 
-
+while(isNaN(numero1)|| isNaN(numero2)){
+    var numero1 = parseInt(prompt("ingrese el primer numero real",0));
+    var numero2 = parseInt(prompt("ingrese segundo numero real",0));
+}
 if(numero1 == numero2){
-    document.write("son identicos");
+    console.log("son identicos");
 }else if (numero1 > numero2){
-    document.write(numero1 + " es mayor que " +  numero2);
+    console.log(numero1 + " es mayor que " +  numero2);
 }else if (numero1 < numero2){
-    document.write( numero1 + " es menor que " + numero2);
+    console.log( numero1 + " es menor que " + numero2);
 }else{
-    document.write("ingreso de caracteres distintos a numeros");
+    console.log("ingreso de caracteres distintos a numeros");
 };
 
