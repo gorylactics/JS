d33aec92551dc15e19c73d7be3cb8196c86d9b58
adrian <adrian.miranda.r@gmail.com>
2019-01-08 00:58:27 -0300

    simple calculator

---
+++ holaMundo/assets/js/18-ejercicio8.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var numero1 = parseInt(prompt('ingresa la primera cifra' , 0))
+var numero2 = parseInt(prompt('ingresa la segunda cifra' , 0))
+
+while(isNaN(numero1 && numero2)){
+    numero1 = parseInt(prompt('ingresa la primera cifra' , 0))
+    numero2 = parseInt(prompt('ingresa la segunda cifra' , 0))
+};
+
+var resultado = 'la suma es = '+(numero1+numero2)+'<br/>'+
+                'la resta es = '+(numero1-numero2)+'</br>'+
+                'la multiplicacion es = '+(numero1*numero2)+'</br>'+
+                'la division es = '+(numero1%numero2);
+
+var resultadoAlert = 'la suma es = '+(numero1+numero2)+'\n'+
+                'la resta es = '+(numero1-numero2)+'\n'+
+                'la multiplicacion es = '+(numero1*numero2)+'\n'+
+                'la division es = '+(numero1%numero2);        
+alert(resultadoAlert);
+document.write(resultado);
