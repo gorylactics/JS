4a99d51a81977d2ce35e405851a6ab707926a888
adrian <adrian.miranda.r@gmail.com>
2019-01-12 19:23:09 -0300

    overwrite

--- holaMundo/assets/js/21-funciones_anonimas.js
+++ holaMundo/assets/js/21-funciones_anonimas.js
@@ -10,11 +10,22 @@ function sumame(numero1 , numero2 , sumaYmuestra, sumaPordos){
     sumaYmuestra(sumar); //RECIBE EL RESULTADO DE LA VARIABLE SUMAR
     sumaPordos(sumar);   //RECIBE EL RESULTADO DE LA VARIABLE SUMAR
 
-};
+    if(sumaYmuestra != 12){
+        console.log('agregando logica');
+    }else
+        console.log('no es igual ');
+    for(sumaYmuestra=9 ; sumaYmuestra<12; sumaYmuestra++)
+        document.write('<h1>'+ sumaYmuestra + '</h1>');
+        
+
+}; // hasta este puntoes la logica de la funcion
 
 // aca llamamos la funcion donde cada funcion tiene un parametro propio en este caso el parametro es DATO
 // DATO recibe el resultado de la VARIABLE SUMAR 
 // DATO = AL RESULTADO DE LA VARIABLE SUMAR
+
+// de aqui hacuia abajo es el proceso de la funcion logica que esta arriba, el como se ejecuta
+
 sumame(5 , 7 , function (dato) {
     console.log('la suma es : ' , dato); //ACA DATO ES EL RESULTADO PURO DE SUMAR
 },
