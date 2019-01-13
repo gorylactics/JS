f3e3c38431f26cdd1b8c355e4ec7f7b2614a1e7d
adrian <adrian.miranda.r@gmail.com>
2019-01-13 20:40:40 -0300

    multidimensional array

---
+++ holaMundo/assets/js/26-array-multidimensional.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var cine= ['chileno' , 'argentino'];
+var peliculas = ['sexo con amor', 'que pena tu vida','pajaros volando', 'cualquier cosa']
+
+var opciones = [cine , peliculas];
+
+console.log(opciones[0][1]);
+console.log(opciones[1][3]);
+console.log(opciones[0][0] )
+
