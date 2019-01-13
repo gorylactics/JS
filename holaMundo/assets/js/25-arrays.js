6fc99851ffa318a0c12ec488f4e47fa5b318d0d9
adrian <adrian.miranda.r@gmail.com>
2019-01-13 19:36:47 -0300

    arrays

---
+++ holaMundo/assets/js/25-arrays.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// son conocidos como arreglos , matrices 
+// es una variable con muchos datos 
+// un array dentro de un array es un red multidimensional
+//  se pueden mesclar los tipos de datos 
+// hay dos formas de hacer un array
+
+var ejemplo1= [1,2,3,'etc'];
+var ejemplo2= new Array(1,2,3,'etc');
+
+// los array se miden por indice el cual comienza por 0
+// para acceder a un dato especifico de un array cuando hacemos el llamado en el nombre de la variable se especifica mediante corchetes
+
+console.log(ejemplo1[3]); 
