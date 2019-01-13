977234d195c610f11bcc7747b5580e5f6bca48a2
adrian <adrian.miranda.r@gmail.com>
2019-01-13 19:54:38 -0300

    run array whit for loop

--- holaMundo/assets/js/25-arrays.js
+++ holaMundo/assets/js/25-arrays.js
@@ -12,10 +12,16 @@ var ejemplo2= new Array(1,2,3,'etc');
 // los array se miden por indice el cual comienza por 0
 // para acceder a un dato especifico de un array cuando hacemos el llamado en el nombre de la variable se especifica mediante corchetes
 
-var elemento = parseInt(prompt('que numero de elemento quieres ' , 0));
+/* var elemento = parseInt(prompt('que numero de elemento quieres ' , 0));
 
 if(elemento >= ejemplo1.length){
     alert('escoge un numero menor a ' + ejemplo1.length );
 }else{
     alert('el elemento es ' + ejemplo1[elemento]);
-};
+}; */
+
+// para recorrer un array podemos usar los loops
+
+for (var i = 0 ; i < ejemplo1.length ; i++){
+    console.log(ejemplo1[i]);
+}
