b8ae51a57220a2604403ddec0296cf35911a42df
adrian <adrian.miranda.r@gmail.com>
2019-01-13 20:02:38 -0300

    foreach

--- holaMundo/assets/js/25-arrays.js
+++ holaMundo/assets/js/25-arrays.js
@@ -7,7 +7,7 @@
 // hay dos formas de hacer un array
 
 var ejemplo1= [1,2,3,'etc'];
-var ejemplo2= new Array(1,2,3,'etc');
+var ejemplo2= new Array(4,5,6,'etcccc');
 
 // los array se miden por indice el cual comienza por 0
 // para acceder a un dato especifico de un array cuando hacemos el llamado en el nombre de la variable se especifica mediante corchetes
@@ -22,6 +22,10 @@ if(elemento >= ejemplo1.length){
 
 // para recorrer un array podemos usar los loops
 
-for (var i = 0 ; i < ejemplo1.length ; i++){
-    console.log(ejemplo1[i]);
-}
+// for (var i = 0 ; i < ejemplo1.length ; i++){
+//     console.log(ejemplo1[i]);
+// }
+
+ejemplo1.forEach(ejemplo1=> {
+    console.log(ejemplo1);
+});
