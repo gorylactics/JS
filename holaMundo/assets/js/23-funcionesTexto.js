c1ec396dda2c7a4e8ca9b6bcd5771324a7754f73
adrian <adrian.miranda.r@gmail.com>
2019-01-12 22:05:59 -0300

    text functions

---
+++ holaMundo/assets/js/23-funcionesTexto.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// transformaciones de texto
+
+var numero = 444;
+var texto1 = 'primer texto';
+var texto2= 'segundo texto';
+var dato = numero.toString();
+
+console.log(typeof(dato));
+
+// calcular longitud de texto
+console.log(texto1.length);
+
+var array=['hola' ,'chao' ,'nos vemos'];
+console.log(array.length); //tambien sirve para contar los elementos dentro de un array
