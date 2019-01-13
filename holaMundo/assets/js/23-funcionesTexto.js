f762419fc87d199681ed7667b7a9545016263e47
adrian <adrian.miranda.r@gmail.com>
2019-01-12 22:21:29 -0300

    text functions2

--- holaMundo/assets/js/23-funcionesTexto.js
+++ holaMundo/assets/js/23-funcionesTexto.js
@@ -5,9 +5,11 @@
 var numero = 444;
 var texto1 = 'primer texto';
 var texto2= 'segundo texto';
+var textoTotal = texto1.concat(' ; ' + texto2); //concat es para concatenar cosas
 var dato = numero.toString();
 
 console.log(typeof(dato));
+console.log(textoTotal);
 
 // calcular longitud de texto
 console.log(texto1.length);
