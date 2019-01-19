7152a27f96b38e8eb9392bd7fcd56f306ff90489
adrian <adrian.miranda.r@gmail.com>
2019-01-19 18:32:02 -0300

    first step arrays exercise

---
+++ holaMundo/assets/js/27-ejercicioArrays.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var num1 = prompt(parseInt('ingresa el primer numero'));
+var num2 = prompt(parseInt('ingresa el segundo numero'));
+var num3 = prompt(parseInt('ingresa el tercer numero'));
+var num4 = prompt(parseInt('ingresa el cuarto numero'));
+var num5 = prompt(parseInt('ingresa el quinto numero'));
+var num6 = prompt(parseInt('ingresa el sexto numero'));
