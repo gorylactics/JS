32ab110595228b4f80ba42938cac8f7f71fc94e5
adrian <adrian.miranda.r@gmail.com>
2019-01-19 18:52:09 -0300

    diferents form to do the same step

--- holaMundo/assets/js/27-ejercicioArrays.js
+++ holaMundo/assets/js/27-ejercicioArrays.js
@@ -1,8 +1,11 @@
 'use strict'
 
-var num1 = prompt(parseInt('ingresa el primer numero'));
-var num2 = prompt(parseInt('ingresa el segundo numero'));
-var num3 = prompt(parseInt('ingresa el tercer numero'));
-var num4 = prompt(parseInt('ingresa el cuarto numero'));
-var num5 = prompt(parseInt('ingresa el quinto numero'));
-var num6 = prompt(parseInt('ingresa el sexto numero'));
+// var numeros = [6];
+// var numeros = new Array(6);
+var numeros = [];
+
+for(var i = 0; i<=5 ;i++){
+    numeros[i] = parseInt(prompt('inserta un numero'));
+};
+
+console.log(numeros);
