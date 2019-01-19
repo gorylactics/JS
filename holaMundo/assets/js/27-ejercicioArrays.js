eb226558fd8e0e4cdb99454dfcd471d657c5e197
adrian <adrian.miranda.r@gmail.com>
2019-01-19 19:16:38 -0300

    run a array

--- holaMundo/assets/js/27-ejercicioArrays.js
+++ holaMundo/assets/js/27-ejercicioArrays.js
@@ -5,7 +5,12 @@
 var numeros = [];
 
 for(var i = 0; i<=5 ;i++){
-    numeros[i] = parseInt(prompt('inserta un numero'));
+    // numeros[i] = parseInt(prompt('inserta un numero'));
+    numeros.push(parseInt(prompt('ingresa un numero', 0)));
 };
+
+numeros.forEach((numeros,index, datos)=>{
+    document.write('<br>' + numeros + '<br/>');
+})
 
 console.log(numeros);
