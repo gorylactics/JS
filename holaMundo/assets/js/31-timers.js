f7c7fc2a9748d36e30fcbf065a28552743a26b5b
adrian <adrian.miranda.r@gmail.com>
2019-01-27 18:02:50 -0300

    my first animation :P

--- holaMundo/assets/js/31-timers.js
+++ holaMundo/assets/js/31-timers.js
@@ -2,5 +2,15 @@
 
 window.addEventListener('load', function(){
     // timers
+    var tiempo = setInterval(function(){
+        console.log('set intervar ejecutado')
 
+        var encabezado = document.querySelector('#titulo');
+        if(titulo.style.fontSize == '50px'){
+            titulo.style.fontSize = '20px';
+        }else{
+            titulo.style.fontSize = '50px'
+        };
+
+    }, 300)
 });
