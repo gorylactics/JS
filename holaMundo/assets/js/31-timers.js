97424897c328176bce71e8e03c3e47a70567cab3
adrian <adrian.miranda.r@gmail.com>
2019-01-27 22:55:14 -0300

    insertion btn start  k

--- holaMundo/assets/js/31-timers.js
+++ holaMundo/assets/js/31-timers.js
@@ -2,18 +2,25 @@
 
 window.addEventListener('load', function(){
     // timers
-    var tiempo = setInterval(function(){
-        console.log('set intervar ejecutado')
 
-        var encabezado = document.querySelector('#titulo');
-        if(titulo.style.fontSize == '50px'){
-            titulo.style.fontSize = '20px';
-        }else{
-            titulo.style.fontSize = '50px'
-        };
+    function intervalo(){
+        
+        var tiempo = setInterval(function(){
+            console.log('set intervar ejecutado')
+    
+            var encabezado = document.querySelector('#titulo');
+            if(titulo.style.fontSize == '50px'){
+                titulo.style.fontSize = '20px';
+            }else{
+                titulo.style.fontSize = '50px'
+            };
+    
+        }, 700);
+        
+        return tiempo;
+    };
 
-    }, 700);
-
+    var tiempo = intervalo();
     var stop = document.querySelector('#stop');
 
     stop.addEventListener('click', function(){
@@ -22,8 +29,10 @@ window.addEventListener('load', function(){
 
     });
     var start = document.querySelector('#start');
+    
     start.addEventListener('click' , function(){
         alert('activaste el bucle');
-        clearInterval(tiempo);
+        intervalo();
+        console.log(start);
     });
 }); 
